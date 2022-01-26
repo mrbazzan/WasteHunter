35ae72b888762f7de3213036c20376309221dc24
Bazzan <bazzanapalowow@gmail.com>
2022-01-26 01:48:51 +0100

    project-structure

---
+++ src/main/java/com/example/demo/WasteHunterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteHunterApplication.class, args);
+	}
+
+}
